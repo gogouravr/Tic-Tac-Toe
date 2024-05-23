e710f1bd99971b5fc4f6c372b27a5191729b96f6
gouravrajbit <gouravr244@gmail.com>
2024-05-23 22:33:11 +0530

    Setup click functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
 import { useState } from 'react';
 import Player from './components/Player/Player';
 import GameBoard from './components/GameBoard/GameBoard';
-import './App.css'
+import './App.css';
+
+class Cell {
+  constructor(x, y, symbol = null) {
+    this.symbol = symbol;
+    this.position = {
+      x, y
+    }
+  }
+}
+
+function getFreshBoard() {
+  return [
+    [new Cell(0, 0), new Cell(0, 1), new Cell(0, 2)],
+    [new Cell(1, 0), new Cell(1, 1), new Cell(1, 2)],
+    [new Cell(2, 0), new Cell(2, 1), new Cell(2, 2)]
+  ]
+}
+
+function updateBoard(board, rowIdx, colIdx, symbol) {
+  // deep clone the board
+  const newBoard = board.map(row => row.map(cell => new Cell(cell.x, cell.y, cell.symbol)))
+  // update the cell
+  newBoard[rowIdx][colIdx]['symbol'] = symbol;
+  return newBoard;
+}
+
+function togglePlaying(playing) {
+  return playing === 'X' ? 'O' : 'X';
+}
 
 function App() {
+
+  const [board, setBoard] = useState(getFreshBoard());
+  let [playing, setPlaying] = useState('X');
+
+  const clickHandler = (rowIdx, colIdx) => {
+    setBoard(oldBoard => {
+      return updateBoard(oldBoard, rowIdx, colIdx, playing);
+    });
+    setPlaying(togglePlaying(playing));
+  }
+
 
   return (
     <div className='main-block'>
@@ -13,7 +53,7 @@ function App() {
       <main>
         <Player />
         <Player />
-        <GameBoard />
+        <GameBoard board={board} clickHandler={clickHandler} />
       </main>
     </div>
   )
--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,5 +1,12 @@
-export default function GameBoard() {
+/* eslint-disable react/prop-types */
+export default function GameBoard({ board, clickHandler }) {
     return (
-        <div>GameBoard</div>
+        <div className="board">
+            {board.map((row, rowIdx) =>
+                row.map((cell, colIdx) =>
+                    <span onClick={() => clickHandler(rowIdx, colIdx)} className="cell" key={`${rowIdx}${colIdx}`}>
+                        {cell.symbol || 'Click'}
+                    </span>))}
+        </div>
     )
 }
